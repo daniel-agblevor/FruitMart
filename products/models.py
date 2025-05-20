6d7d0fa6183f41a093a274889d43e3d327fe506f
daniel-agblevor <mcdanagb@gmail.com>
2025-05-20 16:20:21 +0000

    First commit: Django E-commerce app setup

---
+++ products/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField(max_length=5)
+
+
+    def __str__(self):
+        return self.code
